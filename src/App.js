1592258d08329960d55e6c8eefcd032919db2982
DiegoArvz <darvz.gtz@gmail.com>
2020-03-20 15:20:27 -0600

    Added Animation Intro

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.scss';
 
+import Intro from './components/Intro/Intro';
 import Nav from './components/Nav/Nav';
 import Cover from './components/Cover/Cover';
 import Services from './components/Services/Services';
@@ -8,16 +9,38 @@ import Agreements from './components/Agreements/Agreements';
 import Footer from './components/Footer/Footer';
 import LineSeaparator from './components/LineSeparator/LineSeparator';
 
-const App = () => (
-  <div className="App">
-    <Nav />
-    <Cover />
-    <LineSeaparator />
-    <Services />
-    <LineSeaparator />
-    <Agreements />
-    <Footer />
-  </div>
-)
+const App = () => { 
+
+  const [site, setSite] = useState(true);
+
+  useEffect(() => {
+
+    setTimeout(() => {
+      setSite(false);
+    }, 2000)
+
+    return () => {
+      console.log('will unmount');
+    }
+  }, [])
+
+  return (
+    <div className="App">
+      {
+        site 
+        ? <Intro />
+        : <div className="Site">
+            <Nav />
+            <Cover />
+            <LineSeaparator />
+            <Services />
+            <LineSeaparator />
+            <Agreements />
+            <Footer />
+          </div>
+      }
+    </div>
+  )
+}
 
 export default App;
